dfd14b67afd818d9e4ee1ed7bee56cc45aabfbb2
Neil Italia <neilitalia1998@gmail.com>
2021-09-20 12:10:18 -0500

    setup node server

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const AuthRouter = require('./routes/AuthRouter')
+const ReviewRouter = require('./routes/ReviewRouter')
+const app = express()
+
+const PORT = process.env.PORT || 3001
+
+app.use(cors())
+app.use(logger('dev'))
+app.use(bodyParser.json())
+
+app.use('/auth', AuthRouter)
+app.use('/reviews', ReviewRouter)
+
+app.listen(PORT, () => console.log(`Server Running On Port: ${PORT}`))
