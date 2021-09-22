b9d9db9d2bd0d74490d7c1cdd1008b16d243915c
Neil Italia <neilitalia1998@gmail.com>
2021-09-22 12:10:35 -0500

    style navbar

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import './Nav.css'
 
 export default function Nav() {
   return (
-    <div>
+    <div className="Nav">
       <nav className="navbar">
+        <p>Reviewr</p>
         <NavLink className="navlink" to="/">
           Home
         </NavLink>
