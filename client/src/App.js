c4b8c396aabb2e80218c8ba7cfd879b7a258cb59
Neil Italia <neilitalia1998@gmail.com>
2021-09-21 20:35:42 -0500

    fix addreviews bug

--- client/src/App.js
+++ client/src/App.js
@@ -51,7 +51,10 @@ function App() {
             component={() => <Home user={user} authenticated={authenticated} />}
           />
           <Route exact path="/about" component={About} />
-          <Route path="/addreview/:album_id" component={AddReview} />
+          <Route
+            path="/addreview/:album_id"
+            component={(props) => <AddReview {...props} />}
+          />
           <Route
             exact
             path="/login"
