61a504e0bce366771ec90aa4a5dab86642599848
Hayden Anderson <anderson.hayden@gmail.com>
2021-09-23 17:03:10 -0400

    login styled

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { LoginUser } from '../services/Auth'
+import './Login.css'
 
 export default function Login(props) {
   const [username, setUsername] = useState(null)
@@ -19,27 +20,36 @@ export default function Login(props) {
   }
 
   return (
-    <div>
-      <h2>Log in. Rock Out.</h2>
-      <form className="login-form" onSubmit={handleSubmit}>
-        <label>Username:</label>
-        <input
-          className="username-login"
-          type="text"
-          name="username"
-          required
-          onChange={(e) => setUsername(e.target.value)}
-        ></input>
-        <label>Password:</label>
-        <input
-          className="password-login"
-          type="password"
-          name="password"
-          required
-          onChange={(e) => setPassword(e.target.value)}
-        ></input>
-        <button className="login-button">Login</button>
-      </form>
+    <div className="login-grid">
+      <div className="login-image-container">
+        <img className="signup-image" src="https://i.imgur.com/XFthwBW.jpg" />
+      </div>
+      <div className="login-form-container">
+        <h2>Log in. Rock Out.</h2>
+        <form className="login-form" onSubmit={handleSubmit}>
+          <div className="input1">
+            <label>Username:</label>
+            <input
+              className="username-login"
+              type="text"
+              name="username"
+              required
+              onChange={(e) => setUsername(e.target.value)}
+            ></input>
+          </div>
+          <div className="input2">
+            <label>Password:</label>
+            <input
+              className="password-login"
+              type="password"
+              name="password"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
+          </div>
+          <button className="login-button">Login</button>
+        </form>
+      </div>
     </div>
   )
 }
