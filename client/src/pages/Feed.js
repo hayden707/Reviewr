95b92da1c435ac41d90abc53fe30f330e7bc4f26
Hayden Anderson <anderson.hayden@gmail.com>
2021-09-23 12:14:35 -0400

    updated feed css

--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -20,23 +20,37 @@ export default function Feed() {
         reviews.map((review) => (
           <div>
             <div className="card" key={review.id}>
-              <div>
-                <h3 className="left">{review.album.title}</h3>
-                <h3 className="left">{review.album.artist}</h3>
-                <Link to={`/albumreviews/${review.album.id}`}>
-                  <img
-                    src={review.album.image}
-                    className="album-image"
-                    className="left"
-                    alt="review"
-                  />
-                </Link>
-                <Link to={`/userprofile/${review.user.id}`}>
-                  <h3>by {review.user.username}</h3>
-                </Link>
+              <div className="grid-layout">
+                <div className="album-container">
+                  <h3 id="album-title" className="left">
+                    {review.album.title}
+                  </h3>
+                  <h3 id="artist-name" className="left">
+                    {review.album.artist}
+                  </h3>
+                  <Link to={`/albumreviews/${review.album.id}`}>
+                    <img
+                      src={review.album.image}
+                      className="album-image"
+                      className="left"
+                      alt="review"
+                    />
+                  </Link>
+                </div>
+                <div className="review-container">
+                  <div className="rating-user-bar">
+                    <p className="score">
+                      {parseFloat(review.rating).toFixed(1)}
+                    </p>
+                    <Link to={`/userprofile/${review.user.id}`}>
+                      <h3>by {review.user.username}</h3>
+                    </Link>
+                  </div>
+                  <div className="text-container">
+                    <p>{review.content.substring(0, 80)}</p>
+                  </div>
+                </div>
               </div>
-              <p>{parseFloat(review.rating).toFixed(1)}</p>
-              <p>{review.content.substring(0, 80)}</p>
             </div>
           </div>
         ))}
