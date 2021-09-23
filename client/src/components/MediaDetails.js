d6f9384cb7ab0accd7010b27c90de9e1efd48dfa
msiroilem <twjones86@gmail.com>
2021-09-23 13:03:26 -0400

    MediaDetails content added

--- client/src/components/MediaDetails.js
+++ client/src/components/MediaDetails.js
@@ -1,5 +1,19 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import './MediaDetails.css'
 
-export default function MediaDetails() {
-  return <div></div>
+export default function MediaDetails(props) {
+  return (
+    // <div>
+    <div className="media-card">
+      <NavLink to={`/albumdetails/${props.id}`}>
+        <div key={props.id} className="card">
+          <h3>{props.title}</h3>
+          <p>{props.artist.name}</p>
+          <img src={props.cover_medium} alt="album cover" />
+        </div>
+      </NavLink>
+    </div>
+    // </div>
+  )
 }
