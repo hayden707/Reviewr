9a1e77e96345628e0ee9fb80143b613286f9d110
Hayden Anderson <anderson.hayden@gmail.com>
2021-09-23 16:33:45 -0400

    signup page

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { RegisterUser } from '../services/Auth'
+import './Signup.css'
 
 const iState = {
   email: '',
@@ -27,46 +28,63 @@ export default function Signup(props) {
 
   return (
     <div>
-      <h2>Let's get started.</h2>
-      <form className="signup-form" onSubmit={handleSubmit}>
-        <label>Email:</label>
-        <input
-          className="email-signup"
-          type="email"
-          name="email"
-          value={formValues.email}
-          onChange={handleChange}
-          required
-        ></input>
-        <label>Username:</label>
-        <input
-          className="username-signup"
-          type="text"
-          name="username"
-          value={formValues.username}
-          onChange={handleChange}
-          required
-        ></input>
-        <label>Password:</label>
-        <input
-          className="password-signup"
-          type="password"
-          name="password"
-          value={formValues.password}
-          onChange={handleChange}
-          required
-        ></input>
-        <label>Confirm Password:</label>
-        <input
-          className="confirm-password-signup"
-          type="password"
-          name="confirmPassword"
-          value={formValues.confirmPassword}
-          onChange={handleChange}
-          required
-        ></input>
-        <button className="signup-button">Signup Now</button>
-      </form>
+      <div className="signup-grid">
+        <div className="image-container">
+          <img className="signup-image" src="https://i.imgur.com/XFthwBW.jpg" />
+        </div>
+        <div className="signup-form-container">
+          <h2>Let's get started.</h2>
+          <form className="signup-form" onSubmit={handleSubmit}>
+            <div className="inputs" className="input1">
+              <label>Email:</label>
+              <input
+                className="email-signup"
+                type="email"
+                name="email"
+                value={formValues.email}
+                onChange={handleChange}
+                required
+              ></input>
+            </div>
+            <div className="inputs" className="input2">
+              <label>Username:</label>
+              <input
+                className="username-signup"
+                type="text"
+                name="username"
+                value={formValues.username}
+                onChange={handleChange}
+                required
+              ></input>
+            </div>
+            <div className="inputs" className="input3">
+              <label>Password:</label>
+              <input
+                className="password-signup"
+                type="password"
+                name="password"
+                value={formValues.password}
+                onChange={handleChange}
+                required
+              ></input>
+            </div>
+            <div className="inputs" className="input4">
+              <label>Confirm Password:</label>
+              <input
+                className="confirm-password-signup"
+                type="password"
+                name="confirmPassword"
+                value={formValues.confirmPassword}
+                onChange={handleChange}
+                required
+              ></input>
+            </div>
+            <div className="inputs" className="input5">
+              <button className="signup-button">Signup Now</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
