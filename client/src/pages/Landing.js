3e03260c90b492fb38dc26c0bb66357f17652a20
Neil Italia <neilitalia1998@gmail.com>
2021-09-23 20:49:18 -0500

    add video assets

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Landing.css'
+import Video from '../assets/video.mp4'
 
 export default function Landing() {
   return (
     <div className="landing-grid">
-      <div className="landing-image-container">
-        <img className="landing-image" src="https://i.imgur.com/XFthwBW.jpg" />
+      <div className="landing-video-container">
+        <video autoPlay loop muted>
+          <source src={Video} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </div>
       <div className="landing-text-container">
         <div className="landing-text-flex">
